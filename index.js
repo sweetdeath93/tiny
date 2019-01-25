ffc617c81bc65643e91b96722ed793fd421b04e3
Winardi Susanto <winardisusanto@ymail.com>
2019-01-25 14:59:51 +0700

    Add node module?

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+'use strict'
+
+
+var https = require('https');
+
 module.exports = function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
