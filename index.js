fbfaeb0e88c58faa0bfd464f042735b51ba0b86d
Winardi Susanto <winardisusanto@ymail.com>
2019-01-25 16:28:13 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 'use strict'
 
-
 var https = require('https');
-
-// module.exports = function tiny(string) {
-//     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-//     return string.replace(/\s/g, "");
-// };
 
 module.exports = function tiny(options) {
     const agent = new https.Agent(options);
