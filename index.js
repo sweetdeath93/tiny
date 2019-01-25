5002469a7198d9ea3d90f2320c5d03fba33482c3
Winardi Susanto <winardisusanto@ymail.com>
2019-01-25 15:07:34 +0700

    Add https agent

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 
 var https = require('https');
 
-module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-    return string.replace(/\s/g, "");
+// module.exports = function tiny(string) {
+//     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+//     return string.replace(/\s/g, "");
+// };
+
+module.exports = function tiny(options) {
+    const agent = new https.Agent(options);
+    return agent;
 };
